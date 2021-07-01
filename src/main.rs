a543538dff51adda9261b5ae64260dcfeaf92df8
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-07-01 18:50:35 -0500

    Add the header

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use clap::{App, Arg};
-use ureq::Agent;
 use std::time::Duration;
+use ureq::Agent;
 
-fn main() -> Result<(),ureq::Error>{
+fn main() -> Result<(), ureq::Error> {
     let app = App::new("Furl the Ferris URL Client")
         .version("0.1.0")
         .author("TeoDev1611")
@@ -15,13 +15,22 @@ fn main() -> Result<(),ureq::Error>{
                 .takes_value(true)
                 .required(true),
         )
+        .arg(
+            Arg::with_name("header")
+                .short("H")
+                .long("Header")
+                .help("Pass the header for the http request")
+                .takes_value(true)
+        )
         .get_matches();
-      let agent: Agent = ureq::AgentBuilder::new()
-          .timeout_read(Duration::from_secs(5))
-          .timeout_write(Duration::from_secs(5))
-          .build();
+    let agent: Agent = ureq::AgentBuilder::new()
+        .timeout_read(Duration::from_secs(5))
+        .timeout_write(Duration::from_secs(5))
+        .build();
     let url = app.value_of("url").unwrap_or("https://httpbin.org/get");
     let req = agent.get(url).call()?.into_string()?;
+    let headers = app.value_of("header").unwrap_or("Content-A");
+    println!("{}",headers);
     println!("{}", req);
     Ok(())
 }
