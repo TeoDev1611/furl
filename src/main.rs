a73fd55fffd513d02543323a2c316a8d704d799c
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-07-01 20:43:56 -0500

    Add the response header

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{App, Arg};
+use colored::*;
 use colored_json::ToColoredJson;
 use std::time::Duration;
 use ureq::Agent;
@@ -13,15 +14,14 @@ fn main() -> Result<(), ureq::Error> {
                 .short("U")
                 .long("url")
                 .help("Pass the url for the http request")
-                .takes_value(true)
-                .required(true),
+                .takes_value(true), //.required(true),
         )
         .arg(
             Arg::with_name("header")
                 .short("H")
                 .long("Header")
                 .help("Pass the header for the http request")
-                .takes_value(true)
+                .takes_value(true),
         )
         .get_matches();
     let agent: Agent = ureq::AgentBuilder::new()
@@ -29,14 +29,19 @@ fn main() -> Result<(), ureq::Error> {
         .timeout_write(Duration::from_secs(5))
         .build();
     let url = app.value_of("url").unwrap_or("https://httpbin.org/get");
-    let headers = app.value_of("header").unwrap_or("Application-json").split("-");
+    let headers = app
+        .value_of("header")
+        .unwrap_or("Application-json")
+        .split("-");
+
     let vec = headers.collect::<Vec<&str>>();
     let req = agent.get(url).set(vec[0], vec[1]).call()?.into_string()?;
+    println!("{}", "RESPONSE DATA: \n".green());
     color_json(req);
     Ok(())
 }
 
-fn color_json(text: String) -> ::std::result::Result<(), Box<::std::error::Error>>{
+fn color_json(text: String) -> ::std::result::Result<(), Box<::std::error::Error>> {
     println!("{}", text.to_colored_json_auto()?);
     Ok(())
 }
